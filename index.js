2b720be24d2b88b76315f3e02c7d1e87e41bc12c
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-03-19 11:05:11 +0100

    chalk

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const wildersName = ["Adam", "Anthony", "Aude", "Maxime"];
+
+console.log(chalk.blue(wildersName[0]));
+console.log(chalk.red(wildersName[1]));
+console.log(chalk.green(wildersName[2]));
+console.log(chalk.yellow(wildersName[3]));
